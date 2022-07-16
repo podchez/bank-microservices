e215b42c0d8fc9bd1e477a65ae255d60f7e38905
podchez <maxpodchez@gmail.com>
2022-07-16 14:27:44 +0300

    Correct bill-service

--- bill-service/src/main/java/com/podchez/billservice/repository/BillRepository.java
+++ bill-service/src/main/java/com/podchez/billservice/repository/BillRepository.java
@@ -3,5 +3,9 @@ package com.podchez.billservice.repository;
 import com.podchez.billservice.model.Bill;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface BillRepository extends CrudRepository<Bill, Long> {
+
+    List<Bill> findAllByAccountId(Long accountId);
 }
